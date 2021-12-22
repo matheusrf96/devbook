0c8523f567ac39d3992aab6c855e08441ab0db23
Matheus Rodrigues <matheusrf96@gmail.com>
2021-12-22 19:43:17 -0300

    Implement db package

---
+++ api/db/db.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"api/src/config"
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+func Connect() (*sql.DB, error) {
+	db, err := sql.Open("postgres", config.DatabaseConnectionString)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
